3525866518acc038d266d450bf37c46b5e9d51e5
vivek-0408 <45450102+vivek-0408@users.noreply.github.com>
2025-02-16 11:32:45 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -100,7 +100,7 @@ window.addEventListener('load', () => {
         turnOnLightsBtn.style.opacity = 0; // Fade out the button
         showDecorateButton();
     });
-    console.log("i am there for uuuu");
+    console.log("i am there for youu");
    /* 
     const turnOnLightsBtn = document.getElementById('turn-on-lights');
     console.log(turnOnLightsBtn); // Check if the button is found in the DOM
