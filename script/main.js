351f3d7e11243ac45f4fa41765c7a18817daaffe
vivek-0408 <45450102+vivek-0408@users.noreply.github.com>
2025-02-16 18:57:29 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -84,7 +84,7 @@ const decorationImage = document.getElementById("decorationImage");
     
     /*turnOnLightsBtn.addEventListener('click', () => {
     // Change the screen background color to peach
-        console.log("buttonClicked");
+        console.log("buttonClick");
     gsap.to(document.body, 1, { backgroundColor: "#ffcc99", ease: "power2.inOut" });
 
     // Hide the button after activation
